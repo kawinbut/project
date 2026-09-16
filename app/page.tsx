7acd1af82b2f1fd0b203fa3386852f14d10ab6a7
kanisonsookcharoen <kanison.sookcharoen@gmail.com>
2026-09-16 14:43:21 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,12 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="container py-5">
-      <div className="text-center">
-
-        <h1 className="display-4 fw-bold">
-          Welcome to MyApp
-        </h1>
-
-        <p className="lead text-muted">
-          Next.js + Bootstrap
-        </p>
-
-        <div className="mt-4">
-          <a href="/products" className="btn btn-primary btn-lg">
-            View Products
-          </a>
-        </div>
-
-      </div>
+    <div >
+    
+      <h2> ยินดีต้อนรับเข้าสู่เว็บไซต์ </h2>
+      <p> โดย คณิศร สุขเจริญ </p>
+ 
     </div>
   );
 }
